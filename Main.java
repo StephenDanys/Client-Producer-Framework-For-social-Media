b3c97ccc627dfdf5f4cd48c2e77565ce8fba2253
StephenDanys <giorgosmeid@gmail.com>
2021-04-04 14:22:41 +0300

    made a main class

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class SimpleThread extends Thread {
+	String name;
+
+	public SimpleThread(String input) {
+		this.name = input;
+	}
+
+	public void run() {
+		
+		
+	}
+}
+
+public static void main(String[] args) {
+}
+	
