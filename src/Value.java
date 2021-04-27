1bde4bcbdcf1e9ef306c3e823a3ef9db62967d69
StephenDanys <giorgosmeid@gmail.com>
2021-04-27 19:34:15 +0300

    Classes Ready

---
+++ src/Value.java
@@ -0,0 +1,3 @@
+public class Value {
+    VideoFile videoFile;
+}
---
+++ src/VideoFile.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+public class VideoFile {
+    String videoName;
+    String channelName;
+    String dateCreated;
+    String length;
+    String framerate;
+    String frameWidth;
+    String frameHeight;
+    ArrayList<String> associatedHashtags;
+    byte[] videoFileChunk;
+}
